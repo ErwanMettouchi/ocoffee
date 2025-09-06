537383eb2f20c2bb72bbac8f24348049b32de9a9
Erwan Mettouchi <36893161+ErwanMettouchi@users.noreply.github.com>
2025-09-06 13:15:37 +0200

    Update database client configuration for SSL

--- app/database.js
+++ app/database.js
@@ -2,7 +2,10 @@ import pg from 'pg';
 
 const { Client } = pg;
 
-const client = new Client(process.env.PG_URL);
+const client = new Client({
+	connectionString: process.env.PG_URL,
+	ssl: { rejectUnauthorized: false },
+});
 
 await client.connect();
 
